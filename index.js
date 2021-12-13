5d60ddca15ef19c7f4373f1628007723ceabe172
Djosc <davidjoscaldwell@gmail.com>
2021-12-13 14:53:02 -0500

    Add CORS

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080'];
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            let message = 'The CORS policy for this application doesn\'t allow access from origin '
+                + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 let auth = require('./auth.js')(app);
 
 const passport = require('passport');
