12d534838c3f5f628c1d8adf41d87eac8bde13be
Djosc <davidjoscaldwell@gmail.com>
2021-12-16 19:41:24 -0500

    Testing empty cors call

--- index.js
+++ index.js
@@ -16,18 +16,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = ['*'];
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            let message = 'The CORS policy for this application doesn\'t allow access from origin '
-                + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
+// let allowedOrigins = ['*'];
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             let message = 'The CORS policy for this application doesn\'t allow access from origin '
+//                 + origin;
+//             return callback(new Error(message), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
+app.use(cors());
 
 let auth = require('./auth.js')(app);
 
